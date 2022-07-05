14e933d3c6fa3da875a125f63d3befc9e610c691
HugoNunes87 <hugolima87@hotmail.com>
2022-07-05 20:13:24 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import './App.css';
-import { MonitorPlay } from "phosphor-react";
+import { DiscordLogo, GithubLogo, LinkedinLogo, MonitorPlay } from "phosphor-react";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <img src="https://avatars.githubusercontent.com/u/86809007?v=4" alt="" />
       <h1>Hugo Nunes</h1>
-      <p>@HugoNunes87</p>
+      <p className='username'>@HugoNunes87</p>
       <ul>
         <li>
           <a
             href="https://plataforma-de-video.vercel.app/"
-            target="_blanck"> <MonitorPlay size={30} color="#2b29a8"/> Plataforma de Video - Portfólio</a>
+            target="_blanck"> <MonitorPlay size={30} color="#000000"/> Plataforma de Video - Portfólio</a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/hugo-nunes-323a41164/"
+            target="_blanck"><LinkedinLogo size={30} color="#000000"/>LinkeIn</a>
         </li>
         <li>
           <a
             href=""
-            target="_blanck"></a>
+            target="_blanck"><GithubLogo size={30} color="#000000"/>GitHub</a>
         </li>
         <li>
           <a
-            href=""
-            target="_blanck"></a>
-        </li>
-        <li>
-          <a
-            href=""
-            target="_blanck"></a>
+            href="https://discord.com/"
+            target="_blanck"><DiscordLogo size={30} color="#000000"/>Discord</a>
         </li>
       </ul>
       
